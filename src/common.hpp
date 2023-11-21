fd21d47616eaa08ee1451cca5b87557262925d5a
Henry <henry_yang1997@hotmail.com>
2023-11-20 16:53:12 -0800

    build changes

--- src/common.hpp
+++ src/common.hpp
@@ -53,7 +53,7 @@ static int window_height_px;
 // Actual screen resolution stores iniside render_system as window_resolution
 const ivec2 target_resolution = { 1200, 800 };	// alternative: { 1620, 1080 }
 const ivec2 aspect_ratio = { 3, 2 };			// Remember to reduce this to lowest form!
-// const bool windowed_mode = true;
+const bool windowed_mode = true;
 
 const int tile_size_px = 50;		// Represents how many pixels a tile occupies in a row and column
 
