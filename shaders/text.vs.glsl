8a408600e6e575b3829f6a0491efc68b06c97d93
cplotado <cplotado@students.cs.ubc.ca>
2023-12-01 02:43:30 -0800

    Changed mat4s into mat3s because we're working in 2D

--- shaders/text.vs.glsl
+++ shaders/text.vs.glsl
@@ -2,10 +2,12 @@
 layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
 out vec2 TexCoords;
 
-uniform mat4 projection;
+uniform mat3 projectionMatrix;
+uniform mat3 viewMatrix;
 
 void main()
 {
-    gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
+    vec3 pos = projectionMatrix * viewMatrix * vec3(vertex.xy, 1.0);
+    gl_Position = vec4(pos.xy, 1.0, 1.0);
     TexCoords = vertex.zw;
 } 
