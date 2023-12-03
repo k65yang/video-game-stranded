7a569e2788dfdf971368d4759c71722f85fcd124
Tyler Won <won.tyler1@gmail.com>
2023-12-03 00:02:45 -0800

    create quest system

---
+++ src/quest_system.cpp
@@ -0,0 +1,29 @@
+#include "quest_system.hpp"
+
+void QuestSystem::step(float elapsed_ms) {
+
+};
+
+void QuestSystem::init(RenderSystem* render_system_arg) {
+
+};
+
+void QuestSystem::resetQuestSystem(std::vector<QUEST_ITEM_STATUS> status) {
+
+};
+
+void QuestSystem::foundQuestItem(ITEM_TYPE type) {
+
+};
+
+void QuestSystem::submitQuestItems() {
+
+};
+
+Entity QuestSystem::createQuestItemIndicator(vec2 position, TEXTURE_ASSET_ID texture) {
+    return Entity();
+};
+
+void QuestSystem::submitQuestItem(ITEM_TYPE type) {
+
+};
---
+++ src/quest_system.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include "common.hpp"
+#include "tiny_ecs.hpp"
+#include "components.hpp"
+#include "render_system.hpp"
+#include "tiny_ecs_registry.hpp"
+
+class QuestSystem
+{
+    public:
+        /// @brief QuestSystem constructor
+        QuestSystem()
+        {
+        }
+
+        /// @brief Function to update quest system in world time
+        /// @param elapsed_ms How many ms elapsed since last update
+        void step(float elapsed_ms);
+
+        /// @brief Initializes the render system that the quest system uses
+        /// @param renderer_arg Pointer to the render system
+        void init(RenderSystem* renderer_arg);
+
+        /// @brief Resets the quest system
+        /// @param status Status for quest items
+        void resetQuestSystem(std::vector<QUEST_ITEM_STATUS> status);
+
+        /// @brief Marks a quest item as found and updates its indicator
+        /// @param type The quest item that was found
+        void foundQuestItem(ITEM_TYPE type);
+
+        /// @brief Marks quest item(s) as submitted and updates quest item indicator(s)
+        void submitQuestItems();
+
+    private:
+        RenderSystem* renderer;
+
+        std::map<ITEM_TYPE, TEXTURE_ASSET_ID> quest_item_indicator_not_found_textures_map {
+            {ITEM_TYPE::QUEST_ONE, TEXTURE_ASSET_ID::QUEST_1_NOT_FOUND},
+            {ITEM_TYPE::QUEST_TWO, TEXTURE_ASSET_ID::QUEST_2_NOT_FOUND}
+        };
+        
+        std::map<ITEM_TYPE, TEXTURE_ASSET_ID> quest_item_indicator_found_textures_map {
+            {ITEM_TYPE::QUEST_ONE, TEXTURE_ASSET_ID::QUEST_1_FOUND},
+            {ITEM_TYPE::QUEST_TWO, TEXTURE_ASSET_ID::QUEST_2_FOUND}
+        };
+
+        std::map<ITEM_TYPE, TEXTURE_ASSET_ID> quest_item_indicator_submitted_textures_map {
+            {ITEM_TYPE::QUEST_ONE, TEXTURE_ASSET_ID::QUEST_1_SUBMITTED},
+            {ITEM_TYPE::QUEST_TWO, TEXTURE_ASSET_ID::QUEST_2_SUBMITTED}
+        };
+
+        /// @brief Creates a quest item indicator
+        /// @param position The position of the indicator
+        /// @param texture The texture to use for the indicator
+        Entity createQuestItemIndicator(vec2 position, TEXTURE_ASSET_ID texture);
+
+        /// @brief Marks a quest item as submitted and updates its indicator
+        /// @param type The quest item that was submitted
+        void submitQuestItem(ITEM_TYPE type);
+};
