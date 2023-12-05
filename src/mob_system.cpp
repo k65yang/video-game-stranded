e9bbccb51d2e4749a851589393081fdd7e3fa895
Cameron Mayhew <camjmayhew@gmail.com>
2023-12-05 15:33:48 -0800

    Got health bars to appear! yay :)

--- src/mob_system.cpp
+++ src/mob_system.cpp
@@ -100,19 +100,21 @@ Entity MobSystem::create_mob(vec2 mob_position, MOB_TYPE mob_type, int current_h
 	mob_info.damage = mob_damage_map.at(mob_type);
 	mob_info.aggro_range = mob_aggro_range_map.at(mob_type);
 	mob_info.is_tracking_player = false;
+	vec2 health_position = { mob_position.x, mob_position.y + 1 };
 	if (current_health != 0) {
 		mob_info.health = current_health;
+		// Create a health bar for the mob
+		mob_info.health_bar = create_mob_health_bar(renderer, health_position, current_health, mob_type);
 	}
 	else {
 		mob_info.health = mob_health_map.at(mob_type);
+		// Create a health bar for the mob
+		mob_info.health_bar = create_mob_health_bar(renderer, health_position, mob_health_map.at(mob_type), mob_type);
 	}
 	mob_info.speed_ratio = mob_speed_ratio_map.at(mob_type);
 	mob_info.curr_cell = terrain->get_cell(motion.position);
 	mob_info.type = mob_type;
 
-	// Create a health bar for the mob
-	vec2 health_position = { mob_position.x, mob_position.y + 1 };
-	mob_info.health_bar = create_mob_health_bar(renderer, mob_position, current_health, mob_type);
 
 	// Initialize the collider
 	physics->createMeshCollider(entity, GEOMETRY_BUFFER_ID::MOB001_MESH, renderer);
@@ -156,16 +158,16 @@ Entity MobSystem::create_mob_health_bar(RenderSystem* renderer, vec2 position, i
 	auto& motion = registry.motions.emplace(entity);
 	motion.angle = 0.f;
 	motion.velocity = { 0.f, 0.f };
-	motion.position = { position.x + 2, position.y + 2 };
+	motion.position = position;
 	motion.scale = vec2(((float)amount / (float)mob_health_map.at(type)) * 5.5, 0.7);
 
 	registry.renderRequests.insert(
 		entity,
 		{
-			TEXTURE_ASSET_ID::REDBLOCK,
+			TEXTURE_ASSET_ID::RED_BLOCK,
 			EFFECT_ASSET_ID::TEXTURED,
 			GEOMETRY_BUFFER_ID::SPRITE,
-			RENDER_LAYER_ID::LAYER_1
+			RENDER_LAYER_ID::LAYER_4
 		}
 	);
 
